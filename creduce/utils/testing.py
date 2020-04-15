c1c53f7588bff0c2c87606fccbb621c278ddea22
Martin Liska <mliska@suse.cz>
2020-04-15 16:03:48 +0200

    Proper migration to new more elegant ProcessPool.

--- creduce/utils/testing.py
+++ creduce/utils/testing.py
@@ -23,7 +23,6 @@ import signal
 import subprocess
 import sys
 import tempfile
-import threading
 import weakref
 
 import concurrent.futures
@@ -42,52 +41,30 @@ from .error import PassBugError
 from .error import ZeroSizeError
 
 class TestEnvironment:
-    def __init__(self, test_script, timeout, save_temps, order):
+    def __init__(self, test_script, timeout, save_temps, order, folder):
         self.test_case = None
         self.additional_files = set()
         self.state = None
-        self._dir = tempfile.mkdtemp(prefix="creduce-")
+        self.folder = folder
         self.timeout = timeout
         self.save_temps = save_temps
         self._base_size = None
         self.test_script = test_script
-        self.__exitcode = None
-        self.__process = None
+        self.exitcode = None
+        self.result = None
         self.order = order
-
-# TODO
-#        if not save_temps:
-#            self._finalizer = weakref.finalize(self, self._cleanup, self.path)
 
     def __enter__(self):
         return self
-
-    def __exit__(self, exc, value, tb):
-        if not self.save_temps:
-            self.cleanup()
-
-    @classmethod
-    def _cleanup(cls, name):
-        try:
-            # TODO: remove
-            assert 'creduce-' in name and 'tmp' in name
-            shutil.rmtree(name)
-        except FileNotFoundError:
-            pass
-
-    def cleanup(self):
-        if self._finalizer.detach():
-            self._cleanup(self.path)
-
     def copy_files(self, test_case, additional_files):
         if test_case is not None:
             self.test_case = os.path.basename(test_case)
-            shutil.copy(test_case, self.path)
+            shutil.copy(test_case, self.folder)
             self._base_size = os.path.getsize(test_case)
 
         for f in additional_files:
             self.additional_files.add(os.path.basename(f))
-            shutil.copy(f, self.path)
+            shutil.copy(f, self.folder)
 
     @property
     def size_improvement(self):
@@ -97,16 +74,16 @@ class TestEnvironment:
             return (self._base_size - os.path.getsize(self.test_case_path))
 
     @property
-    def path(self):
-        return self._dir
-
-    @property
     def test_case_path(self):
-        return os.path.join(self.path, self.test_case)
+        return os.path.join(self.folder, self.test_case)
 
     @property
     def additional_files_paths(self):
-        return [os.path.join(self.path, f) for f in self.additional_files]
+        return [os.path.join(self.folder, f) for f in self.additional_files]
+
+    @property
+    def success(self):
+        return self.result == PassResult.OK and self.exitcode == 0
 
     def dump(self, dst):
         if self.test_case is not None:
@@ -123,7 +100,7 @@ class TestEnvironment:
             cmd.append(self.test_case_path)
         cmd.extend(self.additional_files_paths)
 
-        return subprocess.run(cmd, cwd=self.path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return subprocess.run(cmd, cwd=self.folder, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 class TestRunner:
     def __init__(self, test_script, timeout, save_temps):
@@ -141,14 +118,14 @@ class TestRunner:
                 return False
         return True
 
-    def create_environment(self, order):
-        return TestEnvironment(self.test_script, self.timeout, self.save_temps, order)
+    def create_environment(self, order, folder):
+        return TestEnvironment(self.test_script, self.timeout, self.save_temps, order, folder)
 
 class TestManager:
     GIVEUP_CONSTANT = 50000
     MAX_CRASH_DIRS = 10
     MAX_EXTRA_DIRS = 25000
-    MAX_FUTURES = 16
+    MAX_FUTURES = 4
 
     def __init__(self, test_runner, pass_statistic, test_cases, parallel_tests, no_cache, skip_key_off, silent_pass_bug, die_on_pass_bug, print_diff, max_improvement, no_give_up, also_interesting):
         self.test_runner = test_runner
@@ -257,9 +234,11 @@ class TestManager:
     def check_sanity(self):
         logging.debug("perform sanity check... ")
 
-        test_env = self.test_runner.create_environment(0)
+        # TODO: release
+        folder = tempfile.mkdtemp(prefix="creduce-")
+        test_env = self.test_runner.create_environment(0, folder)
 
-        logging.debug("sanity check tmpdir = {}".format(test_env.path))
+        logging.debug("sanity check tmpdir = {}".format(test_env.folder))
 
         test_env.copy_files(None, self.test_cases)
 
@@ -269,72 +248,120 @@ class TestManager:
         else:
             raise InsaneTestCaseError(self.test_cases, p.args)
 
-    def create_and_run_test_env(self, state, order):
-        test_env = self.test_runner.create_environment(order)
+    def create_and_run_test_env(self, state, order, folder):
+        test_env = self.test_runner.create_environment(order, folder)
         # Copy files from base env
-        test_env.copy_files(self._base_test_env.test_case_path, self._base_test_env.additional_files_paths)
+        test_env.copy_files(self.current_test_case, self._base_test_env.additional_files_paths)
         # Copy state from base_env
         test_env.state = state
 
         # transform by state
         (result, test_env.state) = self._pass.transform(test_env.test_case_path, test_env.state)
-        if result != PassResult.OK:
-            return (result, test_env)
+        test_env.result = result
+        if test_env.result != PassResult.OK:
+            return test_env
 
         # run test script
         p = test_env.run_test()
-        self.__exitcode = p.returncode
+        test_env.exitcode = p.returncode
+        return test_env
 
-        return (PassResult.OK if self.__exitcode == 0 else PassResult.INVALID, test_env)
+    @classmethod
+    def release_folder(cls, future, temporary_folders):
+        name = temporary_folders.pop(future)
+        try:
+            # TODO: remove
+            assert 'creduce-' in name and 'tmp' in name
+            shutil.rmtree(name)
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def release_folders(cls, futures, temporary_folders):
+        for future in futures:
+            cls.release_folder(future, temporary_folders)
+        assert not any(temporary_folders)
+
+    def process_done_futures(self, futures, temporary_folders):
+        have_candidate = False
+        new_futures = []
+        for future in futures:
+            if future.done():
+                if future.exception():
+                    # TODO
+                    print(str(future.exception()))
+                    asdf
+                test_env = future.result()
+                if test_env.success:
+                    # TODO check for max_improvement
+                    # TODO check for size change
+                    have_candidate = True
+                    new_futures.append(future)
+                else:
+                    if test_env.result == PassResult.OK:
+                        assert test_env.exitcode
+                        # TODO: also interesting check
+                    elif test_env.result == PassResult.STOP:
+                        # TODO: stop it
+                        have_candidate = True
+                    elif test_env.result == PassResult.ERROR:
+                        # TODO: report error
+                        assert False
+                    else:
+                        # TODO: test_env.order GIVEUP
+                        pass
+                    # TODO
+                    self.release_folder(future, temporary_folders)
+            else:
+                new_futures.append(future)
+
+        return (have_candidate, new_futures)
+
+    def wait_for_first_success(self, futures):
+        for future in futures:
+            test_env = future.result()
+            if test_env.success:
+                return test_env
+        return None
+
+    @classmethod
+    def terminate_all(cls, pool):
+        pool.stop()
+        pool.join()
 
     def run_parallel_tests(self):
         with ProcessPool(max_workers=self.parallel_tests) as pool:
             futures = []
+            temporary_folders = {}
             order = 1
             while self._base_test_env.state != None:
                 # do not create too many states
                 if len(futures) >= self.MAX_FUTURES:
                     wait(futures, return_when=FIRST_COMPLETED)
 
-                done = set([future for future in futures if future.done()])
-                candidate = None
-                for future in done:
-                    (result, test_env) = future.result()
-                    if result == PassResult.OK or result == PassResult.STOP or result == PassResult.ERROR:
-                        candidate = future
-                        break
+                (have_candidate, futures) = self.process_done_futures(futures, temporary_folders)
+                if have_candidate:
+                    success = self.wait_for_first_success(futures)
+                    self.terminate_all(pool)
+                    return (success, futures, temporary_folders)
 
-                # wait for all before the candidate
-                if candidate:
-                    for f in futures[:futures.index(candidate)]:
-                        f.result()
-                        (result, _) = future.result()
-                        # TODO: add method for it
-                        if result == PassResult.OK or result == PassResult.STOP or result == PassResult.ERROR:
-                            break
-
-                    # we joined all futures before the candidate, let's close the pool
-                    pool.close()
-                    pool.stop()
-                    pool.join()
-                    return [candidate]
-
-                futures = [f for f in futures if not f in done]
-                futures.append(pool.schedule(self.create_and_run_test_env, (self._base_test_env.state, order)))
+                folder = tempfile.mkdtemp(prefix="creduce-")
+                future = pool.schedule(self.create_and_run_test_env, [self._base_test_env.state, order, folder])
+                temporary_folders[future] = folder
+                futures.append(future)
                 order += 1
-                state = self._pass.advance(self._base_test_env.test_case_path, self._base_test_env.state)
+                state = self._pass.advance(self.current_test_case, self._base_test_env.state)
                 # we are at the end of enumeration
                 if state == None:
-                    pool.close()
-                    pool.join()
-                    return futures
+                    success = self.wait_for_first_success(futures)
+                    self.terminate_all(pool)
+                    return (success, futures, temporary_folders)
                 else:
                     self._base_test_env.state = state
 
-            assert False
-
     def run_pass(self, pass_):
         self._pass = pass_
+        self.futures = []
 
         logging.info("===< {} >===".format(self._pass))
 
@@ -342,7 +369,7 @@ class TestManager:
             raise zerosizeerror(self.test_cases)
 
         for test_case in self.test_cases:
-            self._current_test_case = test_case
+            self.current_test_case = test_case
 
             if self._get_file_size([test_case]) == 0:
                 continue
@@ -362,7 +389,9 @@ class TestManager:
                         continue
 
             # Create initial test environment
-            self._base_test_env = self.test_runner.create_environment(0)
+            # TODO: remove
+            folder = tempfile.mkdtemp(prefix="creduce-")
+            self._base_test_env = self.test_runner.create_environment(0, folder)
             self._base_test_env.copy_files(test_case, self.test_cases ^ {test_case})
             self._base_test_env.state = self._pass.new(self._base_test_env.test_case_path)
 
@@ -381,23 +410,18 @@ class TestManager:
                         logging.info("****** skipping the rest of this pass ******")
 
                 # TODO: XXX
-                parallel_tests = self.run_parallel_tests()
-                finished = [future for future in parallel_tests if future.done() and not future.cancelled()]
-                if not finished:
+                success_env, futures, temporary_folders = self.run_parallel_tests()
+                if not success_env:
                     return
 
-                candidates = [f.result()[1] for f in finished if f.result()[0] == PassResult.OK]
-                candidates = sorted(candidates, key = lambda c: c.order)
-                if candidates:
-                    self.process_result(candidates[0])
-                else:
-                    return
+                self.process_result(success_env)
+                self.release_folders(futures, temporary_folders)
 
     def process_result(self, test_env):
         logging.debug("Process result")
 
         self._base_test_env = test_env
-        shutil.copy(test_env.test_case_path, self._current_test_case)
+        shutil.copy(test_env.test_case_path, self.current_test_case)
         state = self._pass.advance_on_success(test_env.test_case_path, self._base_test_env.state)
 
         if state is not None:
