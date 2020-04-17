a404b7cb0057d2dca9a8fc3c3c514fed7ad10329
Martin Liska <mliska@suse.cz>
2020-04-17 15:59:15 +0200

    Port IntsPass.

--- creduce/tests/test_ints.py
+++ creduce/tests/test_ints.py
@@ -21,7 +21,7 @@ class IntsATestCase(unittest.TestCase):
 
         os.unlink(tmp_file.name)
 
-        self.assertEqual(variant, "Compute 23L + 0x456 + 0789!\n")
+        self.assertEqual(variant, "Compute 123L + 0x56 + 0789!\n")
 
     def test_success_a(self):
         with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_file:
@@ -30,10 +30,11 @@ class IntsATestCase(unittest.TestCase):
         state = self.pass_.new(tmp_file.name)
         (result, state) = self.pass_.transform(tmp_file.name, state)
 
-        iteration = 0
-
-        while result == PassResult.OK and iteration < 6:
+        iteration = 1
+        while result == PassResult.OK and iteration < 10:
             state = self.pass_.advance_on_success(tmp_file.name, state)
+            if state == None:
+                break
             (result, state) = self.pass_.transform(tmp_file.name, state)
             iteration += 1
 
@@ -52,13 +53,15 @@ class IntsATestCase(unittest.TestCase):
         state = self.pass_.new(tmp_file.name)
         (result, state) = self.pass_.transform(tmp_file.name, state)
 
-        iteration = 0
+        iteration = 1
 
-        while result == PassResult.OK and iteration < 4:
+        while result == PassResult.OK and iteration < 10:
             with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_file:
                 tmp_file.write("Compute 123L + 0x456 + 0789!\n")
 
             state = self.pass_.advance(tmp_file.name, state)
+            if state == None:
+                break
             (result, state) = self.pass_.transform(tmp_file.name, state)
             iteration += 1
 
