54b1699e3867c106d7b072244a22d1843beac5bb
Martin Liska <mliska@suse.cz>
2020-04-18 11:38:28 +0200

    Come up with testabstract.py file.

--- creduce/tests/test_peep.py
+++ creduce/tests/test_peep.py
@@ -2,17 +2,9 @@ import os
 import tempfile
 import unittest
 
+from creduce.tests.testabstract import iterate_pass
 from creduce.passes.abstract import PassResult
 from ..passes import PeepPass
-
-def iterate_pass(current_pass, path):
-    state = current_pass.new(path)
-    while state != None:
-        (result, state) = current_pass.transform(path, state)
-        if result == PassResult.OK:
-            state = current_pass.advance_on_success(path, state)
-        else:
-            state = current_pass.advance(path, state)
 
 class PeepATestCase(unittest.TestCase):
     def setUp(self):
